8445b9c1845564a06549fa39038374286ecc92fe
Raul Gonzalez <raulagonzalez@outlook.com>
2020-05-08 18:56:16 -0500

    Updated DAO FILE

--- BasicCrudApp/src/main/java/com/basicapp/dao/ProductManagementDAO.java
+++ BasicCrudApp/src/main/java/com/basicapp/dao/ProductManagementDAO.java
@@ -12,76 +12,85 @@ import com.basicapp.pojo.Product;
 
 public class ProductManagementDAO {
 	public List<Product> getAllProducts() {
+		//create array list
 		List<Product> productList = new ArrayList<Product>();
 		try {
+			//open connection
 			Connection conn = DBUtil.getConnection();
+			//create statement
 			Statement st = conn.createStatement();
-			ResultSet rs = st.executeQuery("Select * From product");
-			while(rs.next())
-			{
-				Product product = new
-						Product(rs.getString("prod_in"),rs.getString("prod_name"),rs.getInt("prod_price"));
+			//create ResultSet O=bject and execute the SQL Query
+			ResultSet rs = st.executeQuery("SELECT * FROM product");
+			while (rs.next()) {
+				Product product = new Product(rs.getString("prod_id"), rs.getString("prod_name"),
+						rs.getInt("prod_price"));
 				productList.add(product);
 			}
+			//close connection 
 			DBUtil.closeConnection(conn);
-			}
-		catch (Exception e)
-		{
+		} catch (Exception e) {
 			e.printStackTrace();
-	}
+		}
+		//return product list 
 		return productList;
-		
-}
-	public Product getProductByID(String productId)
-	{
+	}
+
+	public Product getProductById(String productId) {
 		Product product = null;
-		try
-		{
-			Connection connection = DBUtil.getCOnnection();
+		try {
+			Connection conn = DBUtil.getConnection();
 			PreparedStatement ps = conn.prepareStatement("SELECT * FROM product WHERE prod_id = ?");
 			ps.setString(1, productId);
-			ResulSet rs = ps.executeQuery();
-			while(rs.next())
-			{
-				product = new
-						Product(rs.getString("prod_id"),rs.getString("prod_name"),rs.getInt("prod_price"));
-				
+			ResultSet rs = ps.executeQuery();
+			while (rs.next()) {
+				product = new Product(rs.getString("prod_id"), rs.getString("prod_name"), rs.getInt("prod_price"));
 			}
-		}
-		catch(Exception e)
-		{
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
 		return product;
 	}
-	public int addProduct(Product product)
-	{
-		int status =0;
+
+	public int addProduct(Product product) {
+		int status = 0;
 		try {
 			Connection conn = DBUtil.getConnection();
-			PreparedStatement ps = conn.prepareStatement("Update product SET prod_name=?,prd_price=? WHERE prod_id=?");
+			PreparedStatement ps = conn.prepareStatement("INSERT INTO product VALUES(?,?,?)");
+			ps.setString(1, product.getProductId());
+			ps.setString(2, product.getProductName());
+			ps.setInt(3, product.getProductPrice());
+			status = ps.executeUpdate();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return status;
+	}
+
+	public int updateProduct(Product product) {
+		int status = 0;
+		try {
+			Connection conn = DBUtil.getConnection();
+			PreparedStatement ps = conn.prepareStatement("UPDATE product SET prod_name=?,prod_price=? WHERE prod_id=?");
 			ps.setString(1, product.getProductName());
 			ps.setInt(2, product.getProductPrice());
 			ps.setString(3, product.getProductId());
 			status = ps.executeUpdate();
-		}
-		catch(Exception e) {
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
 		return status;
 	}
+
 	public int deleteProduct(String productId) {
 		int status = 0;
-		try
-		{
-			Connection conn = DBUtil.getCOnnection();
+		try {
+			Connection conn = DBUtil.getConnection();
 			PreparedStatement ps = conn.prepareStatement("DELETE FROM product where prod_id = ?");
 			ps.setString(1, productId);
 			status = ps.executeUpdate();
-		}
-		catch(Exception e)
-		{
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
 		return status;
 	}
+}
